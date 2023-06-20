e2d0c1d57432cf213d6e805e9a44fd23ccebfc17
remediosronnel <remediosronnel30@gmail.com>
2023-06-21 00:43:30 +0800

    Commit

--- app/src/main/java/com/remedios/fitnessgymapp/MainActivity.kt
+++ app/src/main/java/com/remedios/fitnessgymapp/MainActivity.kt
@@ -12,5 +12,8 @@ class MainActivity:AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+
     }
 }
--- app/src/main/java/com/remedios/fitnessgymapp/SplashScreen.kt
+++ app/src/main/java/com/remedios/fitnessgymapp/SplashScreen.kt
@@ -1,9 +1,11 @@
 package com.remedios.fitnessgymapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 import com.remedios.fitnessgymapp.databinding.ActivitySplashscreenBinding
@@ -17,8 +19,13 @@ class SplashScreen:AppCompatActivity() {
 
         Handler(Looper.getMainLooper()).postDelayed({
             binding.animation1.playAnimation()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }, 9000)
 
 
+
     }
+
 }
+
